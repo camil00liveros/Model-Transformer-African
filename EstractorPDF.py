c168bc025124c3e36bf5c5008d6c9403979f714b
camil00liveros <ocamilo@unicauca.edu.co>
2026-08-03 22:27:05 -0500

    ajustes

--- EstractorPDF.py
+++ EstractorPDF.py
@@ -9,8 +9,6 @@ from PIL import Image
 from tqdm.auto import tqdm
 from google.colab import files
 
-
-
 print('PyMuPDF version:', fitz.__version__)
 
 # ── Cargar PDF ──────────────────────────────────────────────
