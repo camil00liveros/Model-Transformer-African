0f589a1db717ae7a003f5b7a09644f1863e410d0
camil00liveros <ocamilo@unicauca.edu.co>
2026-08-06 21:53:52 -0500

    ajustes

--- EstractorPDF.py
+++ EstractorPDF.py
@@ -11,6 +11,9 @@ from google.colab import files
 
 print('PyMuPDF version:', fitz.__version__)
 
+
+
+
 # ── Cargar PDF ──────────────────────────────────────────────
 uploaded = files.upload()
 PDF_PATH = next(iter(uploaded.keys()))
